cf4a3fff8fd4ab6e0b4e736d4e51b2e2e6bcafb0
Your Name <you@example.com>
2025-08-21 13:53:24 +0530

    normal

--- frontend/job-portal/src/utils/apiPaths.js
+++ frontend/job-portal/src/utils/apiPaths.js
@@ -36,6 +36,6 @@ export const API_PATHS = {
   },
 
   IMAGE: {
-    UPLOAD_IMAGE: "/api/auth/uploads/", // Upload profile picture
+    UPLOAD_IMAGE: "/api/auth/upload-image", // Upload profile picture
   },
 };
